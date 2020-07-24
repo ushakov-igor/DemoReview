bd8350b3a761034172cd6a62e6e30952d5c04cd8
Anita Kivich <zombanita13@yandex.com>
2020-07-24 16:28:42 +0300

    Форматирование

--- webpack.config.js
+++ webpack.config.js
@@ -4,51 +4,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Правила сборки
-
 module.exports = {
   entry: { main: './src/pages/index.js' },
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js'
-      },
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
   module: {
-      rules: [
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
           {
-              test: /\.js$/,
-              loader: 'babel-loader',
-              exclude: '/node_modules/'
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
           },
-          {
-              test: /\.css$/,
-              loader: [
-                  MiniCssExtractPlugin.loader,
-                  {
-                      loader: 'css-loader',
-                      options: {
-                          importLoaders: 1
-                      }
-                  },
-                  'postcss-loader'
-              ],
-          },
-          {
-              test: /\.html$/,
-              loader: 'html-loader',
-          },
-          {
-              test: /\.(png|jpe?g|gif|ico|svg)$/,
-              loader: 'file-loader?name=./images/[name].[ext]',
-          },
-          {
-              test: /\.(eot|ttf|woff|woff2)$/,
-              loader: 'file-loader?name=./fonts/[name].[ext]',
-          }
-      ]
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        loader: 'file-loader?name=./images/[name].[ext]',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./fonts/[name].[ext]',
+      }
+    ]
   },
   plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html'
-      }),
-      new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
   ]
 };
